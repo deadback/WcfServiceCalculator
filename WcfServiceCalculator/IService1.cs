a4add63116fc2b933c6485201140d3679be42f49
bpsou <bpsou@DESKTOP-IDBM2H0>
2016-10-24 21:48:27 +0100

    Add project files.

---
+++ WcfServiceCalculator/IService1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfServiceCalculator
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IService1
+    {
+        [OperationContract]
+        int sum(int a, int b);
+
+        [OperationContract]
+        int sub(int a, int b);
+
+        [OperationContract]
+        double mul(double a, double b);
+
+        [OperationContract]
+        float div(float a, float b);
+
+        // TODO: Add your service operations here
+    }
+
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+}
